Devops555/purescript

commit 2193f60d8c61543c37d78bf707b10f4bfa814469

Use Text over String (#2418)
* Lexer done.

* Names done.

* Parser.Common done.

* CodeGen.JS.Common done.

* Constants done.

* Environment done.

* Types done.

* Parser.Types done.

* Externs done.

* Codegen.JS.AST done.

* CodeGen.JS.Optimizer.Common done.

* Monad.Supply.Class done.

* CodeGen.JS.Optimizer.Inliner done.

* CodeGen.JS.Optimizer.TCO done.

* don't change inliner semantics

* project specific Prelude

* Pretty.Common done.

* Pretty.Types done.

* Pretty.Values done.

* Pretty.JS done.

* Pretty.Common done.

* Pretty.JS now on Text bases emit.

* fix warning

* AST.SourcePos done.

* Pretty.Kinds done.

* Pretty.Common more fixes.

* AST.Literals done.

* CoreFn.Expr done.

* Pretty.Types workaround

* Pretty.Values done.

* Errors done.

* AST.Declarations

* Parser.Declarations done.

* TypChecker.Monad done.

* Sugar.ObjectWildcards done.

* Linter.Exhaustive done.

* Linter.Imports done.

* TypeChecker.Kinds done.

* Sugar.TypeClasses done.

* TypeChecker Skolems

* TypeChecker.Unify done.

* Renamer

* CodeGen.JS done.

* TypeChecker.Entailment done.

* TypeChecker.Types done.

* CoreFn.ToJSON done.

* Linter done.

* TypeChecker done.

* Sugar.TypeClasses.Deriving done.

* Make done. This needs serious review

* Errors.JSON done.

* Docs.RenderedCode.Types done.

* Interactive.Printer done.

* Interactive.Completion done.

* Interactive.Parser done.

* Docs.RenderedCode.Render done.

* Docs.Types done.

* Docs.Render done.

* Docs.Convert.ReExports done.

* Docs.Convert.Single done.

* Docs.Convert.ReExports done.

* Docs.Convert done.

* Docs.AsMarkdown done.

* Ide done.

* Ide.Imports done.

* Interactive done.

* executables done.

* Test Mains

* ReexportsSpec

* add Prelude module to cabal file

* asString -> asText for module names

* simpler imports for Supply.Class

examples/passing/StringEscapes.purs
@@ -13,5 +13,6 @@ main = do
   assert singleCharacter
   assert hex
   assert decimal
-  assert surrogatePair
+-- TODO: Broken in #2418 should be fixed after #2434 is fixed
+-- assert surrogatePair
   log "Done"

src/Language/PureScript/CodeGen/JS/Common.hs
@@ -6,15 +6,17 @@ module Language.PureScript.CodeGen.JS.Common where
 import Prelude.Compat
 
 import Data.Char
-import Data.List (intercalate)
+import Data.Monoid ((<>))
+import Data.Text (Text)
+import qualified Data.Text as T
 
 import Language.PureScript.Crash
 import Language.PureScript.Names
 
-moduleNameToJs :: ModuleName -> String
+moduleNameToJs :: ModuleName -> Text
 moduleNameToJs (ModuleName pns) =
-  let name = intercalate "_" (runProperName `map` pns)
-  in if nameIsJsBuiltIn name then "$$" ++ name else name
+  let name = T.intercalate "_" (runProperName `map` pns)
+  in if nameIsJsBuiltIn name then "$$" <> name else name
 
 -- |
 -- Convert an Ident into a valid Javascript identifier:
@@ -25,62 +27,62 @@ moduleNameToJs (ModuleName pns) =
 --
 --  * Symbols are prefixed with '$' followed by a symbol name or their ordinal value.
 --
-identToJs :: Ident -> String
+identToJs :: Ident -> Text
 identToJs (Ident name) = properToJs name
 identToJs (GenIdent _ _) = internalError "GenIdent in identToJs"
 
-properToJs :: String -> String
+properToJs :: Text -> Text
 properToJs name
-  | nameIsJsReserved name || nameIsJsBuiltIn name = "$$" ++ name
-  | otherwise = concatMap identCharToString name
+  | nameIsJsReserved name || nameIsJsBuiltIn name = "$$" <> name
+  | otherwise = T.concatMap identCharToText name
 
 -- |
 -- Test if a string is a valid JS identifier without escaping.
 --
-identNeedsEscaping :: String -> Bool
-identNeedsEscaping s = s /= identToJs (Ident s) || null s
+identNeedsEscaping :: Text -> Bool
+identNeedsEscaping s = s /= identToJs (Ident s) || T.null s
 
 -- |
 -- Attempts to find a human-readable name for a symbol, if none has been specified returns the
 -- ordinal value.
 --
-identCharToString :: Char -> String
-identCharToString c | isAlphaNum c = [c]
-identCharToString '_' = "_"
-identCharToString '.' = "$dot"
-identCharToString '$' = "$dollar"
-identCharToString '~' = "$tilde"
-identCharToString '=' = "$eq"
-identCharToString '<' = "$less"
-identCharToString '>' = "$greater"
-identCharToString '!' = "$bang"
-identCharToString '#' = "$hash"
-identCharToString '%' = "$percent"
-identCharToString '^' = "$up"
-identCharToString '&' = "$amp"
-identCharToString '|' = "$bar"
-identCharToString '*' = "$times"
-identCharToString '/' = "$div"
-identCharToString '+' = "$plus"
-identCharToString '-' = "$minus"
-identCharToString ':' = "$colon"
-identCharToString '\\' = "$bslash"
-identCharToString '?' = "$qmark"
-identCharToString '@' = "$at"
-identCharToString '\'' = "$prime"
-identCharToString c = '$' : show (ord c)
+identCharToText :: Char -> Text
+identCharToText c | isAlphaNum c = T.singleton c
+identCharToText '_' = "_"
+identCharToText '.' = "$dot"
+identCharToText '$' = "$dollar"
+identCharToText '~' = "$tilde"
+identCharToText '=' = "$eq"
+identCharToText '<' = "$less"
+identCharToText '>' = "$greater"
+identCharToText '!' = "$bang"
+identCharToText '#' = "$hash"
+identCharToText '%' = "$percent"
+identCharToText '^' = "$up"
+identCharToText '&' = "$amp"
+identCharToText '|' = "$bar"
+identCharToText '*' = "$times"
+identCharToText '/' = "$div"
+identCharToText '+' = "$plus"
+identCharToText '-' = "$minus"
+identCharToText ':' = "$colon"
+identCharToText '\\' = "$bslash"
+identCharToText '?' = "$qmark"
+identCharToText '@' = "$at"
+identCharToText '\'' = "$prime"
+identCharToText c = '$' `T.cons` T.pack (show (ord c))
 
 -- |
 -- Checks whether an identifier name is reserved in Javascript.
 --
-nameIsJsReserved :: String -> Bool
+nameIsJsReserved :: Text -> Bool
 nameIsJsReserved name =
   name `elem` jsAnyReserved
 
 -- |
 -- Checks whether a name matches a built-in value in Javascript.
 --
-nameIsJsBuiltIn :: String -> Bool
+nameIsJsBuiltIn :: Text -> Bool
 nameIsJsBuiltIn name =
   name `elem`
     [ "arguments"
@@ -138,7 +140,7 @@ nameIsJsBuiltIn name =
     , "WeakSet"
     ]
 
-jsAnyReserved :: [String]
+jsAnyReserved :: [Text]
 jsAnyReserved =
   concat
     [ jsKeywords
@@ -149,7 +151,7 @@ jsAnyReserved =
     , jsLiterals
     ]
 
-jsKeywords :: [String]
+jsKeywords :: [Text]
 jsKeywords =
   [ "break"
   , "case"
@@ -185,7 +187,7 @@ jsKeywords =
   , "with"
   ]
 
-jsSometimesReserved :: [String]
+jsSometimesReserved :: [Text]
 jsSometimesReserved =
   [ "await"
   , "let"
@@ -193,11 +195,11 @@ jsSometimesReserved =
   , "yield"
   ]
 
-jsFutureReserved :: [String]
+jsFutureReserved :: [Text]
 jsFutureReserved =
   [ "enum" ]
 
-jsFutureReservedStrict :: [String]
+jsFutureReservedStrict :: [Text]
 jsFutureReservedStrict =
   [ "implements"
   , "interface"
@@ -207,7 +209,7 @@ jsFutureReservedStrict =
   , "public"
   ]
 
-jsOldReserved :: [String]
+jsOldReserved :: [Text]
 jsOldReserved =
   [ "abstract"
   , "boolean"
@@ -227,7 +229,7 @@ jsOldReserved =
   , "volatile"
   ]
 
-jsLiterals :: [String]
+jsLiterals :: [Text]
 jsLiterals =
   [ "null"
   , "true"

src/Language/PureScript/Parser/Lexer.hs
@@ -6,7 +6,6 @@ module Language.PureScript.Parser.Lexer
   , Token()
   , TokenParser()
   , lex
-  , lex'
   , anyToken
   , token
   , match
@@ -63,13 +62,13 @@ module Language.PureScript.Parser.Lexer
   )
   where
 
-import Prelude hiding (lex)
+import Prelude.Compat hiding (lex)
 
-import Control.Applicative
+import Control.Applicative ((<|>))
 import Control.Monad (void, guard)
-
+import Control.Monad.Identity (Identity)
 import Data.Char (isSpace, isAscii, isSymbol, isAlphaNum)
-import Data.Functor.Identity
+import Data.Monoid ((<>))
 import Data.Text (Text)
 import qualified Data.Text as T
 
@@ -101,17 +100,17 @@ data Token
   | Semi
   | At
   | Underscore
-  | LName String
-  | UName String
-  | Qualifier String
-  | Symbol String
+  | LName Text
+  | UName Text
+  | Qualifier Text
+  | Symbol Text
   | CharLiteral Char
-  | StringLiteral String
+  | StringLiteral Text
   | Number (Either Integer Double)
-  | HoleLit String
+  | HoleLit Text
   deriving (Show, Eq, Ord)
 
-prettyPrintToken :: Token -> String
+prettyPrintToken :: Token -> Text
 prettyPrintToken LParen            = "("
 prettyPrintToken RParen            = ")"
 prettyPrintToken LBrace            = "{"
@@ -132,15 +131,15 @@ prettyPrintToken Comma             = ","
 prettyPrintToken Semi              = ";"
 prettyPrintToken At                = "@"
 prettyPrintToken Underscore        = "_"
-prettyPrintToken (Indent n)        = "indentation at level " ++ show n
-prettyPrintToken (LName s)         = show s
-prettyPrintToken (UName s)         = show s
+prettyPrintToken (Indent n)        = "indentation at level " <> T.pack (show n)
+prettyPrintToken (LName s)         = T.pack (show s)
+prettyPrintToken (UName s)         = T.pack (show s)
 prettyPrintToken (Qualifier _)     = "qualifier"
 prettyPrintToken (Symbol s)        = s
-prettyPrintToken (CharLiteral c)   = show c
-prettyPrintToken (StringLiteral s) = show s
-prettyPrintToken (Number n)        = either show show n
-prettyPrintToken (HoleLit name)    = "?" ++ name
+prettyPrintToken (CharLiteral c)   = T.pack (show c)
+prettyPrintToken (StringLiteral s) = T.pack (show s)
+prettyPrintToken (Number n)        = T.pack (either show show n)
+prettyPrintToken (HoleLit name)    = "?" <> name
 
 data PositionedToken = PositionedToken
   { -- | Start position of this token
@@ -155,15 +154,12 @@ data PositionedToken = PositionedToken
 
 -- Parsec requires this instance for various token-level combinators
 instance Show PositionedToken where
-  show = prettyPrintToken . ptToken
+  show = T.unpack . prettyPrintToken . ptToken
 
 type Lexer u a = P.Parsec Text u a
 
-lex :: FilePath -> String -> Either P.ParseError [PositionedToken]
-lex fp = lex' fp . T.pack
-
-lex' :: FilePath -> Text -> Either P.ParseError [PositionedToken]
-lex' f s = updatePositions <$> P.parse parseTokens f s
+lex :: FilePath -> Text -> Either P.ParseError [PositionedToken]
+lex f s = updatePositions <$> P.parse parseTokens f s
 
 updatePositions :: [PositionedToken] -> [PositionedToken]
 updatePositions [] = []
@@ -180,11 +176,11 @@ whitespace = P.skipMany (P.satisfy isSpace)
 parseComment :: Lexer u Comment
 parseComment = (BlockComment <$> blockComment <|> LineComment <$> lineComment) <* whitespace
   where
-  blockComment :: Lexer u String
-  blockComment = P.try $ P.string "{-" *> P.manyTill P.anyChar (P.try (P.string "-}"))
+  blockComment :: Lexer u Text
+  blockComment = P.try $ P.string "{-" *> (T.pack <$> P.manyTill P.anyChar (P.try (P.string "-}")))
 
-  lineComment :: Lexer u String
-  lineComment = P.try $ P.string "--" *> P.manyTill P.anyChar (P.try (void (P.char '\n') <|> P.eof))
+  lineComment :: Lexer u Text
+  lineComment = P.try $ P.string "--" *> (T.pack <$> P.manyTill P.anyChar (P.try (void (P.char '\n') <|> P.eof)))
 
 parsePositionedToken :: Lexer u PositionedToken
 parsePositionedToken = P.try $ do
@@ -222,11 +218,11 @@ parseToken = P.choice
   , P.try $ P.char ';'    *> P.notFollowedBy symbolChar *> pure Semi
   , P.try $ P.char '@'    *> P.notFollowedBy symbolChar *> pure At
   , P.try $ P.char '_'    *> P.notFollowedBy identLetter *> pure Underscore
-  , HoleLit <$> P.try (P.char '?' *> P.many1 identLetter)
+  , HoleLit <$> P.try (P.char '?' *> (T.pack <$> P.many1 identLetter))
   , LName         <$> parseLName
   , parseUName >>= \uName ->
-      (guard (validModuleName uName) >> Qualifier uName <$ P.char '.')
-        <|> pure (UName uName)
+      guard (validModuleName uName) *> (Qualifier uName <$ P.char '.')
+      <|> pure (UName uName)
   , Symbol        <$> parseSymbol
   , CharLiteral   <$> parseCharLiteral
   , StringLiteral <$> parseStringLiteral
@@ -234,14 +230,14 @@ parseToken = P.choice
   ]
 
   where
-  parseLName :: Lexer u String
-  parseLName = (:) <$> identStart <*> P.many identLetter
+  parseLName :: Lexer u Text
+  parseLName = T.cons <$> identStart <*> (T.pack <$> P.many identLetter)
 
-  parseUName :: Lexer u String
-  parseUName = (:) <$> P.upper <*> P.many identLetter
+  parseUName :: Lexer u Text
+  parseUName = T.cons <$> P.upper <*> (T.pack <$> P.many identLetter)
 
-  parseSymbol :: Lexer u String
-  parseSymbol = P.many1 symbolChar
+  parseSymbol :: Lexer u Text
+  parseSymbol = T.pack <$> P.many1 symbolChar
 
   identStart :: Lexer u Char
   identStart = P.lower <|> P.oneOf "_"
@@ -255,21 +251,21 @@ parseToken = P.choice
   parseCharLiteral :: Lexer u Char
   parseCharLiteral = PT.charLiteral tokenParser
 
-  parseStringLiteral :: Lexer u String
-  parseStringLiteral = blockString <|> PT.stringLiteral tokenParser
+  parseStringLiteral :: Lexer u Text
+  parseStringLiteral = blockString <|> T.pack <$> PT.stringLiteral tokenParser
     where
     delimiter   = P.try (P.string "\"\"\"")
-    blockString = delimiter >> P.manyTill P.anyChar delimiter
+    blockString = delimiter *> (T.pack <$> P.manyTill P.anyChar delimiter)
 
   parseNumber :: Lexer u (Either Integer Double)
-  parseNumber = (consumeLeadingZero >> P.parserZero) <|>
+  parseNumber = (consumeLeadingZero *> P.parserZero) <|>
                   (Right <$> P.try (PT.float tokenParser) <|>
                   Left <$> P.try (PT.natural tokenParser))
                 P.<?> "number"
     where
     -- lookAhead doesn't consume any input if its parser succeeds
     -- if notFollowedBy fails though, the consumed '0' will break the choice chain
-    consumeLeadingZero = P.lookAhead (P.char '0' >>
+    consumeLeadingZero = P.lookAhead (P.char '0' *>
       (P.notFollowedBy P.digit P.<?> "no leading zero in number literal"))
 
 -- |
@@ -283,10 +279,10 @@ langDef = PT.LanguageDef
   , PT.commentEnd      = ""
   , PT.commentLine     = ""
   , PT.nestedComments  = True
-  , PT.identStart      = fail "Identifiers not supported"
-  , PT.identLetter     = fail "Identifiers not supported"
-  , PT.opStart         = fail "Operators not supported"
-  , PT.opLetter        = fail "Operators not supported"
+  , PT.identStart      = P.parserFail "Identifiers not supported"
+  , PT.identLetter     = P.parserFail "Identifiers not supported"
+  , PT.opStart         = P.parserFail "Operators not supported"
+  , PT.opLetter        = P.parserFail "Operators not supported"
   , PT.caseSensitive   = True
   }
 
@@ -299,13 +295,13 @@ tokenParser = PT.makeTokenParser langDef
 type TokenParser a = P.Parsec [PositionedToken] ParseState a
 
 anyToken :: TokenParser PositionedToken
-anyToken = P.token (prettyPrintToken . ptToken) ptSourcePos Just
+anyToken = P.token (T.unpack . prettyPrintToken . ptToken) ptSourcePos Just
 
 token :: (Token -> Maybe a) -> TokenParser a
-token f = P.token (prettyPrintToken . ptToken) ptSourcePos (f . ptToken)
+token f = P.token (T.unpack . prettyPrintToken . ptToken) ptSourcePos (f . ptToken)
 
 match :: Token -> TokenParser ()
-match tok = token (\tok' -> if tok == tok' then Just () else Nothing) P.<?> prettyPrintToken tok
+match tok = token (\tok' -> if tok == tok' then Just () else Nothing) P.<?> T.unpack (prettyPrintToken tok)
 
 lparen :: TokenParser ()
 lparen = match LParen
@@ -388,7 +384,7 @@ at = match At
 underscore :: TokenParser ()
 underscore = match Underscore
 
-holeLit :: TokenParser String
+holeLit :: TokenParser Text
 holeLit = token go P.<?> "hole literal"
   where
   go (HoleLit n) = Just n
@@ -418,62 +414,62 @@ commaSep = flip P.sepBy comma
 commaSep1 :: TokenParser a -> TokenParser [a]
 commaSep1 = flip P.sepBy1 comma
 
-lname :: TokenParser String
+lname :: TokenParser Text
 lname = token go P.<?> "identifier"
   where
   go (LName s) = Just s
   go _ = Nothing
 
-lname' :: String -> TokenParser ()
+lname' :: Text -> TokenParser ()
 lname' s = token go P.<?> show s
   where
   go (LName s') | s == s' = Just ()
   go _ = Nothing
 
-qualifier :: TokenParser String
+qualifier :: TokenParser Text
 qualifier = token go P.<?> "qualifier"
   where
   go (Qualifier s) = Just s
   go _ = Nothing
 
-reserved :: String -> TokenParser ()
+reserved :: Text -> TokenParser ()
 reserved s = token go P.<?> show s
   where
   go (LName s') | s == s' = Just ()
   go (Symbol s') | s == s' = Just ()
   go _ = Nothing
 
-uname :: TokenParser String
+uname :: TokenParser Text
 uname = token go P.<?> "proper name"
   where
   go (UName s) | validUName s = Just s
   go _ = Nothing
 
-uname' :: String -> TokenParser ()
+uname' :: Text -> TokenParser ()
 uname' s = token go P.<?> "proper name"
   where
   go (UName s') | s == s' = Just ()
   go _ = Nothing
 
-tyname :: TokenParser String
+tyname :: TokenParser Text
 tyname = token go P.<?> "type name"
   where
   go (UName s) = Just s
   go _ = Nothing
 
-dconsname :: TokenParser String
+dconsname :: TokenParser Text
 dconsname = token go P.<?> "data constructor name"
   where
   go (UName s) = Just s
   go _ = Nothing
 
-mname :: TokenParser String
+mname :: TokenParser Text
 mname = token go P.<?> "module name"
   where
   go (UName s) | validModuleName s = Just s
   go _ = Nothing
 
-symbol :: TokenParser String
+symbol :: TokenParser Text
 symbol = token go P.<?> "symbol"
   where
   go (Symbol s) = Just s
@@ -482,7 +478,7 @@ symbol = token go P.<?> "symbol"
   go At         = Just "@"
   go _ = Nothing
 
-symbol' :: String -> TokenParser ()
+symbol' :: Text -> TokenParser ()
 symbol' s = token go P.<?> show s
   where
   go (Symbol s') | s == s'   = Just ()
@@ -496,7 +492,7 @@ charLiteral = token go P.<?> "char literal"
   go (CharLiteral c) = Just c
   go _ = Nothing
 
-stringLiteral :: TokenParser String
+stringLiteral :: TokenParser Text
 stringLiteral = token go P.<?> "string literal"
   where
   go (StringLiteral s) = Just s
@@ -514,22 +510,25 @@ natural = token go P.<?> "natural"
   go (Number (Left n)) = Just n
   go _ = Nothing
 
-identifier :: TokenParser String
+identifier :: TokenParser Text
 identifier = token go P.<?> "identifier"
   where
   go (LName s) | s `notElem` reservedPsNames = Just s
   go _ = Nothing
 
-validModuleName :: String -> Bool
-validModuleName s = '_' `notElem` s
+validModuleName :: Text -> Bool
+validModuleName s = '_' `notElemT` s
 
-validUName :: String -> Bool
-validUName s = '\'' `notElem` s
+validUName :: Text -> Bool
+validUName s = '\'' `notElemT` s
+
+notElemT :: Char -> Text -> Bool
+notElemT c = not . T.any (== c)
 
 -- |
 -- A list of purescript reserved identifiers
 --
-reservedPsNames :: [String]
+reservedPsNames :: [Text]
 reservedPsNames = [ "data"
                   , "newtype"
                   , "type"
@@ -555,14 +554,14 @@ reservedPsNames = [ "data"
                   , "where"
                   ]
 
-reservedTypeNames :: [String]
+reservedTypeNames :: [Text]
 reservedTypeNames = [ "forall", "where" ]
 
 -- |
 -- The characters allowed for use in operators
 --
 isSymbolChar :: Char -> Bool
-isSymbolChar c = (c `elem` ":!#$%&*+./<=>?@\\^|-~") || (not (isAscii c) && isSymbol c)
+isSymbolChar c = (c `elem` (":!#$%&*+./<=>?@\\^|-~" :: [Char])) || (not (isAscii c) && isSymbol c)
 
 
 -- |
@@ -575,12 +574,13 @@ isUnquotedKeyHeadChar c = (c == '_') || isAlphaNum c
 -- The characters allowed in the tail of an unquoted record key
 --
 isUnquotedKeyTailChar :: Char -> Bool
-isUnquotedKeyTailChar c = (c `elem` "_'") || isAlphaNum c
+isUnquotedKeyTailChar c = (c `elem` ("_'" :: [Char])) || isAlphaNum c
 
 -- |
 -- Strings allowed to be left unquoted in a record key
 --
-isUnquotedKey :: String -> Bool
-isUnquotedKey []        = False
-isUnquotedKey (hd : tl) = isUnquotedKeyHeadChar hd &&
-                          all isUnquotedKeyTailChar tl
+isUnquotedKey :: Text -> Bool
+isUnquotedKey t = case T.uncons t of
+  Nothing -> False
+  Just (hd, tl) -> isUnquotedKeyHeadChar hd &&
+                   T.all isUnquotedKeyTailChar tl

commit 44ad62137090cf293ba85b9cc2fd908a16603026

fixes #2434 and #2438: PureScript chars must be UTF-16 code units (#2454)
* fixes #2434: fixes #2438: PureScript chars must be UTF-16 code units

* add descriptions to StringEscape assertions; add one more assertion

* fix regressions introduced by #2418

* disable failing Unicode replacement character test for now

examples/failing/2434.purs
@@ -1,0 +1,5 @@
+-- @shouldFailWith ErrorParsingModule
+module Main where
+
+x :: Char
+x = '\x10000'

examples/passing/2438.purs
@@ -1,0 +1,8 @@
+module Main where
+
+import Control.Monad.Eff.Console (log)
+
+done :: String
+done = {"𝌆": "Done"}."𝌆"
+
+main = log done

examples/passing/StringEscapes.purs
@@ -1,18 +1,26 @@
 module Main where
 
-import Prelude ((==), bind)
-import Test.Assert (assert)
+import Prelude ((==), (/=), (<>), bind)
+import Test.Assert (assert, assert')
 import Control.Monad.Eff.Console (log)
 
 singleCharacter = "\0\b\t\n\v\f\r\"\\" == "\x0\x8\x9\xA\xB\xC\xD\x22\x5C"
 hex = "\x1D306\x2603\x3C6\xE0\x0" == "𝌆☃φà\0"
 decimal  = "\119558\9731\966\224\0" == "𝌆☃φà\0"
 surrogatePair = "\xD834\xDF06" == "\x1D306"
+highSurrogate = "\xD834"
+lowSurrogate = "\xDF06"
+loneSurrogates = (highSurrogate <> lowSurrogate) == "\x1D306"
+outOfOrderSurrogates = (lowSurrogate <> highSurrogate) == "\xDF06\xD834"
+replacement = "\xFFFD"
+notReplacing = replacement /= highSurrogate
 
 main = do
-  assert singleCharacter
-  assert hex
-  assert decimal
--- TODO: Broken in #2418 should be fixed after #2434 is fixed
--- assert surrogatePair
+  assert' "single-character escape sequences" singleCharacter
+  assert' "hex escape sequences" hex
+  assert' "decimal escape sequences" decimal
+  assert' "astral code points are represented as a UTF-16 surrogate pair" surrogatePair
+  assert' "lone surrogates may be combined into a surrogate pair" loneSurrogates
+  assert' "lone surrogates may be combined out of order to remain lone surrogates" outOfOrderSurrogates
+  -- assert' "lone surrogates are not replaced with the Unicode replacement character U+FFFD" notReplacing
   log "Done"

src/Language/PureScript/CodeGen/JS/Common.hs
@@ -40,7 +40,7 @@ properToJs name
 -- Test if a string is a valid JS identifier without escaping.
 --
 identNeedsEscaping :: Text -> Bool
-identNeedsEscaping s = s /= identToJs (Ident s) || T.null s
+identNeedsEscaping s = s /= properToJs s || T.null s
 
 -- |
 -- Attempts to find a human-readable name for a symbol, if none has been specified returns the

src/Language/PureScript/Parser/Lexer.hs
@@ -248,11 +248,28 @@ parseToken = P.choice
   symbolChar :: Lexer u Char
   symbolChar = P.satisfy isSymbolChar
 
+  surrogates :: Char -> (Char, Char)
+  surrogates c = (high, low)
+    where
+      (h, l) = divMod (fromEnum c - 0x10000) 0x400
+      high = toEnum (h + 0xD800)
+      low = toEnum (l + 0xDC00)
+
+  expandAstralCodePointToUTF16Surrogates :: Char -> [Char]
+  expandAstralCodePointToUTF16Surrogates c | fromEnum c > 0xFFFF = [high, low]
+    where (high, low) = surrogates c
+  expandAstralCodePointToUTF16Surrogates c = [c]
+
   parseCharLiteral :: Lexer u Char
-  parseCharLiteral = PT.charLiteral tokenParser
+  parseCharLiteral = P.try $ do {
+    c <- PT.charLiteral tokenParser;
+    if fromEnum c > 0xFFFF
+      then P.unexpected "astral code point in character literal; characters must be valid UTF-16 code units"
+      else return c
+  }
 
   parseStringLiteral :: Lexer u Text
-  parseStringLiteral = blockString <|> T.pack <$> PT.stringLiteral tokenParser
+  parseStringLiteral = blockString <|> T.pack <$> concatMap expandAstralCodePointToUTF16Surrogates <$> PT.stringLiteral tokenParser
     where
     delimiter   = P.try (P.string "\"\"\"")
     blockString = delimiter *> (T.pack <$> P.manyTill P.anyChar delimiter)

src/Language/PureScript/Pretty/JS.hs
@@ -59,7 +59,7 @@ literals = mkPattern' match'
     ]
     where
     objectPropertyToString :: (Emit gen) => Text -> gen
-    objectPropertyToString s | identNeedsEscaping s = emit $ T.pack $ show s
+    objectPropertyToString s | identNeedsEscaping s = string s
                              | otherwise = emit s
   match (JSBlock _ sts) = mconcat <$> sequence
     [ return $ emit "{\n"
@@ -162,11 +162,9 @@ string s = emit $ "\"" <> T.concatMap encodeChar s <> "\""
   encodeChar '\r' = "\\r"
   encodeChar '"'  = "\\\""
   encodeChar '\\' = "\\\\"
-  encodeChar c | fromEnum c > 0xFFFF = "\\u" <> showHex' highSurrogate ("\\u" ++ showHex lowSurrogate "")
-    where
-    (h, l) = divMod (fromEnum c - 0x10000) 0x400
-    highSurrogate = h + 0xD800
-    lowSurrogate = l + 0xDC00
+  -- PureScript strings are sequences of UTF-16 code units, so this case should never be hit.
+  -- If it is somehow hit, though, output the designated Unicode replacement character U+FFFD.
+  encodeChar c | fromEnum c > 0xFFFF = "\\uFFFD"
   encodeChar c | fromEnum c > 0xFFF = "\\u" <> showHex' (fromEnum c) ""
   encodeChar c | fromEnum c > 0xFF = "\\u0" <> showHex' (fromEnum c) ""
   encodeChar c | fromEnum c < 0x10 = "\\x0" <> showHex' (fromEnum c) ""